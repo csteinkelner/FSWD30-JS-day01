50fac10c10be08223b8e058aad7c6ec87ec41f75
Cornalia Steinkelner <cornelia.steinkelner@chello.at>
2017-11-29 12:01:12 +0100

    todays

---
+++ exercise2-java.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+document.write("Hello my name is Mathias");
+	var age = 25;
+	document.write(" and I am " + age + " Years old");
+
+
+document.write("<br>");
+
+player = ["Martin","Thomas","Peter","Mathias","Niki"];
+var player_name = player[2];
+
+document.write("The most valuable player of the match is " + player_name +"!");
